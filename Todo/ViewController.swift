8db3ffe0fd4044b292d0f7d7ef5c1be67b3a2897
Petr Grishin <petr.grishin@grishini.ru>
2015-02-21 13:28:08 +0300

    Add source data to cell

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath index: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: index) as UITableViewCell
-        cell.textLabel.text = todoList[index.row]
+        cell.textLabel?.text = todoList[index.row]
         return cell
     }
 
