41af8568f85e01e1f259ef048e48f7723bcf4a84
Petr Grishin <petr.grishin@grishini.ru>
2015-02-21 17:16:20 +0300

    Реализация жеста смахивания элемента списка задач

--- Todo/TableViewCell.swift
+++ Todo/TableViewCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    var originalCenter = CGPoint()
+    var deleteOnDragRelease = false
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,4 +23,41 @@ class TableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        var recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        recognizer.delegate = self
+        addGestureRecognizer(recognizer)
+
+    }
+
+    func handlePan(recognizer: UIPanGestureRecognizer) {
+        // 1
+        if recognizer.state == .Began {
+            // when the gesture begins, record the current center location
+            originalCenter = center
+        }
+        // 2
+        if recognizer.state == .Changed {
+            let translation = recognizer.translationInView(self)
+            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+            // has the user dragged the item far enough to initiate a delete/complete?
+            deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+        }
+        // 3
+        if recognizer.state == .Ended {
+            // the frame this cell had before user dragged it
+            let originalFrame = CGRect(x: 0, y: frame.origin.y,
+                    width: bounds.size.width, height: bounds.size.height)
+            if !deleteOnDragRelease {
+                // if the item is not being deleted, snap back to the original location
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+            }
+        }
+    }
 }
